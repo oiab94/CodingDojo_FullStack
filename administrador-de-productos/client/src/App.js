5a2d156fa7213ade57ef0de6a5be31933123be02
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-01-31 18:46:45 -0300

    [Frontend] Creación de un frontend sencillo para envio de datos

---
+++ administrador-de-productos/client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import ProductManager from './components/ProductManager';
+
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <ProductManager />
+    </div>
+  );
+}
+
+export default App;
---
+++ administrador-de-productos/client/src/components/ProductManager.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react';
+import axios from 'axios';
+
+const ProductManager = () => {
+	const [title, setTitle] = useState("");
+  const [price, setPrice] = useState(0);
+  const [description, setDescription] = useState("");
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault();
+		axios.post(
+			"http://localhost:8000/api/newProduct",
+			{title, price, description})
+			.then(res => console.log("Response: ", res))
+			.catch(err => console.log("Error: ", err))
+  }
+
+	return (
+		<>
+			<h1>Product Manager</h1>
+			<form onSubmit={onSubmitHandler}>
+        <p>
+        <label>Title</label>
+        <input 
+          type="text"
+          onChange={e => setTitle(e.target.value)} />
+        </p>
+        <p>
+          <label>Price</label>
+          <input 
+            type="text"
+            onChange={e => setPrice(e.target.value)} />
+        </p>
+        <p>
+          <label>Description</label>
+          <input 
+            type="text"
+            onChange={e => setDescription(e.target.value)} />
+        </p>
+        <input 
+        type="submit" 
+        value="Create"/>
+      </form>
+		</>
+	);
+}
+export default ProductManager;

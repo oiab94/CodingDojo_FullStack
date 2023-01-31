b4cbaa2f3e9eb18f2d96ecabcbd59704a049b207
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-01-31 17:15:37 -0300

    [Backend] Insercion de rutas y recepcion del POST

--- administrador-de-productos/server.js
+++ administrador-de-productos/server.js
@@ -10,6 +10,12 @@ app.listen(port, () => console.log(`Se escucha en el puerto ${port}`));
 
 // * Insertamos los MIDDLEWARE
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 
 // * Conexion a la DB
 require("./server/config/mongoose.config")
+
+// * Conexion a las rutas
+const ruta = require("./server/routes/productManager.route");
+ruta(app);

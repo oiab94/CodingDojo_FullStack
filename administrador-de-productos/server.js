5073089369e483ba6bcf81bb7f9981096c1c2b53
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-01-31 15:49:36 -0300

    [Tarea][BackEnd] Configuración básica para poner a la escucha al server

---
+++ administrador-de-productos/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+
+// * Inicia express
+const app = express();
+const port = 3000;
+
+// * Ponemos a la escucha el servidor
+app.listen(port, () => console.log(`Se escucha en el puerto ${port}`));
+
+// * Insertamos los MIDDLEWARE
+app.use(cors());
